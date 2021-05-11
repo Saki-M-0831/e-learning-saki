117224c0601b3a1f89fb94cf947b8542d026951c
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-11 19:10:28 +0900

    cut unnecesary part

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,4 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    @user = User.find_by(email: params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      login(@user)
-      flash[:success] = "Yay, You're Logged In Now!!"
-
-      redirect_to @user
-    else
-      flash.now[:danger] = "Ooops, Invalid Credentials..."
-
-      render "new"
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-  end
 end
