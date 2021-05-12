371d57cc4491ca1b2c89f5adb276b305426c56a1
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-12 16:26:18 +0900

    [function] sign in (#5)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+
+      login(@user)
+      flash[:success] = "Yay, You're Logged In Now!!"
+
+      redirect_to root_url 
+    else
+      flash.now[:danger] = "Ooops, Invalid Credentials..."
+
+      render "new"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    logout
+
+    flash[:info] = "Logged Out!! See You Again!!"
+    redirect_to new_session_url
+  end
+
 end
