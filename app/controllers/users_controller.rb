357e4a093667ed166d93aa103bde9c1c47e7db09
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-12 17:26:15 +0900

    [function]setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+     redirect_to root_url
+    else
+      render "edit"
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
