0240aedba4ea92ea183e8447a9fe52cbbbfc2776
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-14 20:09:15 +0900

    [layout]member

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all.paginate(page: params[:page], per_page: 6)
+  end
+
   def user_params
     params.require(:user).permit(:picture, :name, :email, :password, :password_confirmation, :is_admin)
   end
