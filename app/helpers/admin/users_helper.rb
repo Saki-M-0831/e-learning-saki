3aca3dabdf7f6cf3e7bd848df7aeb63faa92e619
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-14 17:02:10 +0900

    [function]admin user (#10)
    * [function]admin user
    
    * indention

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,5 @@
 module Admin::UsersHelper
+  def get_bg(user)
+    user.is_admin ? "admin-bg" : "non-admin-bg"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   get '/about', to: "pages#about"
 
   namespace :admin do
-    resources :users
+    resources :users do
+      member do
+        patch "add_admin"
+        patch "remove_admin"
+      end
+    end
   end
+
   namespace :admin do
     get 'pages/home'
   end
