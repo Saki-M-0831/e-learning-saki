c782b900094dc75b2abf17cf03a14b814589100d
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-14 16:46:19 +0900

    [function]admin user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,17 @@ class Admin::UsersController < ApplicationController
     @users = User.all.paginate(page: params[:page], per_page: 6)
   end
 
+  def add_admin
+    @user = User.find(params[:id])
+    @user.update(is_admin: true)
+
+    redirect_to admin_users_url
+  end
+
+  def remove_admin
+    @user = User.find(params[:id])
+    @user.update(is_admin: false)
+
+    redirect_to admin_users_url
+  end
 end
