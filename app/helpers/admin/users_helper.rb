05a5f6e2186efd55ae42b3507a6dd3964aed903f
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-14 17:00:05 +0900

    indention

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,5 +1,5 @@
 module Admin::UsersHelper
-    def get_bg(user)
-        user.is_admin ? "admin-bg" : "non-admin-bg"
-    end
+  def get_bg(user)
+    user.is_admin ? "admin-bg" : "non-admin-bg"
+  end
 end
