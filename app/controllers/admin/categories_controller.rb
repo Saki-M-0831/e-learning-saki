69d0910ff043f3660f0ac19298843f0f8d11545f
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-18 16:07:26 +0900

    [function]admin category (#16)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,42 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(categ_params)
+    if @category.save
+      redirect_to admin_categories_url
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(categ_params)
+      redirect_to admin_categories_url
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to admin_categories_url
+  end
+
+  def categ_params
+    params.require(:category).permit(:title, :description)
   end
 end
