93e44f90818b5b91b24f6f51ea91bec3c3502544
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-19 16:48:08 +0900

    [function]word (#18)
    * [function]word
    
    * simple

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,8 +1,49 @@
 class Admin::WordsController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
+    @words = @category.words
   end
 
   def new
+    @category = Category.find(params[:category_id])
+    @word = Word.new
+    3.times { @word.choices.new }
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new(word_params)
+    if @word.save
+      redirect_to admin_category_words_url
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @word = Word.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:category_id])
+    @word = Word.find(params[:id])
+    if @word.update(word_params)
+      redirect_to admin_category_words_url
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:category_id])
+    @word = Word.find(params[:id])
+    @word.destroy
+
+    redirect_to admin_category_words_url
+  end
+
+  def word_params
+    params.require(:word).permit(:content, choices_attributes: [:id, :content, :is_correct])
   end
 end
