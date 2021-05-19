dbf1ceb40e4b034819a1e33da041ef62bfd11c37
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-19 18:22:46 +0900

    [layout]lesson (#20)

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :categories
+  resources :answers
 end
