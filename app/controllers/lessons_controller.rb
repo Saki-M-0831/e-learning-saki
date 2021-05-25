f71a354ff5d2d71c552f14277659b5c59edc9208
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-25 15:28:38 +0900

    activity log (#26)
    * activity log
    
    * paginate
    
    * addjustments
    
    * simple

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
   def create
     @lesson = current_user.lessons.new(category_id: params[:category_id])
     @lesson.save
+    @lesson.create_activity(user_id:current_user.id)
     
     redirect_to new_lesson_answer_url(@lesson)
   end
