56ac40ae17d2f8becd04931f4177dca54358d49e
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-25 17:33:38 +0900

    simple helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,13 +1,5 @@
 module CategoriesHelper
-  def filter_learned
-    @filter == "learned" ? 'btn-primary' : 'btn-outline-primary'
-  end
-
-  def filter_not_learned
-    @filter == "not learned" ? 'btn-primary' : 'btn-outline-primary'
-  end
-   
-  def filter_all
-    @filter == "all" || @filter.nil? ? 'btn-primary' : 'btn-outline-primary'
+  def filter_class(status)
+    @filter == status ? 'btn-primary' : 'btn-outline-primary'
   end
 end
