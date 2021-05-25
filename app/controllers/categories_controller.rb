53cdc5182083343708e3bcaf470f606d9714497b
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-25 17:13:44 +0900

    filter

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,16 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all.paginate(page: params[:page], per_page: 6)
+    @filter = params[:filter]
     @lessons = Lesson.where(user_id: current_user.id)
+
+    if @filter == "learned"
+      categ_ids = @lessons.pluck(:category_id)
+      @categories = Category.where(id: categ_ids).paginate(page: params[:page], per_page: 6)
+    elsif @filter == "not learned"
+      categ_ids = Category.all.pluck(:id) - @lessons.pluck(:category_id)
+      @categories = Category.where(id: categ_ids).paginate(page: params[:page], per_page: 6)
+    else
+      @categories = Category.paginate(page: params[:page], per_page: 6)
+    end
   end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,13 @@
 module CategoriesHelper
+  def filter_learned
+    @filter == "learned" ? 'btn-primary' : 'btn-outline-primary'
+  end
+
+  def filter_not_learned
+    @filter == "not learned" ? 'btn-primary' : 'btn-outline-primary'
+  end
+   
+  def filter_all
+    @filter == "all" || @filter.nil? ? 'btn-primary' : 'btn-outline-primary'
+  end
 end
