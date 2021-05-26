cd84b27d7152b11ba62eb787765a483d156b39f8
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-26 16:07:15 +0900

    label for button

--- app/controllers/learned_words_controller.rb
+++ app/controllers/learned_words_controller.rb
@@ -1,6 +1,8 @@
 class LearnedWordsController < ApplicationController
   def index
-    @categories = Category.all
-    @words = current_user.word_learned(params[:word_filter])
+    categ_ids = Lesson.where(user_id: current_user.id).pluck(:category_id)
+    @categories = Category.where(id: categ_ids)
+    @words = current_user.word_learned(params[:word_filter]).paginate(page: params[:page], per_page: 6)
+    @w = params[:word_filter]
   end
 end
