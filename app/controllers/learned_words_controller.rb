d899c9fb7e4b0d48de1e9ea76dc5c862b06da28e
Saki-M-0831 <83812192+Saki-M-0831@users.noreply.github.com>
2021-05-26 16:37:27 +0900

    filtering (#28)
    * filtering
    
    * label for button
    
    * move to controller
    
    * clarity

--- app/controllers/learned_words_controller.rb
+++ app/controllers/learned_words_controller.rb
@@ -1,4 +1,9 @@
 class LearnedWordsController < ApplicationController
   def index
+    categ_ids = Lesson.where(user_id: current_user.id).pluck(:category_id)
+    @categories = Category.where(id: categ_ids)
+    @words = current_user.word_learned(params[:word_filter]).paginate(page: params[:page], per_page: 6)
+    
+    @button_name = params[:word_filter] ? Category.find(params[:word_filter]).title : "Categories"
   end
 end
