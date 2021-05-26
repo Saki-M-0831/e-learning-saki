a68d72ebc924eff35c31cb628c88bada8a83b05c
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-26 16:32:57 +0900

    clarity

--- app/controllers/learned_words_controller.rb
+++ app/controllers/learned_words_controller.rb
@@ -3,9 +3,7 @@ class LearnedWordsController < ApplicationController
     categ_ids = Lesson.where(user_id: current_user.id).pluck(:category_id)
     @categories = Category.where(id: categ_ids)
     @words = current_user.word_learned(params[:word_filter]).paginate(page: params[:page], per_page: 6)
-    @w = params[:word_filter]
-
-    w_ids = @words.pluck(:word_id)
-    c_id = Word.where(id: w_ids).pluck(:category_id).first
+    
+    @button_name = params[:word_filter] ? Category.find(params[:word_filter]).title : "Categories"
   end
 end
