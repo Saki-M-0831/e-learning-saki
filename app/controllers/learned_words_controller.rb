17b725368403baad6ff443dba8814fd41ea61d06
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-26 16:20:41 +0900

    move to controller

--- app/controllers/learned_words_controller.rb
+++ app/controllers/learned_words_controller.rb
@@ -4,5 +4,8 @@ class LearnedWordsController < ApplicationController
     @categories = Category.where(id: categ_ids)
     @words = current_user.word_learned(params[:word_filter]).paginate(page: params[:page], per_page: 6)
     @w = params[:word_filter]
+
+    w_ids = @words.pluck(:word_id)
+    c_id = Word.where(id: w_ids).pluck(:category_id).first
   end
 end
