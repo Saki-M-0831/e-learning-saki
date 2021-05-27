dbee33e1101c85c0faded898e5e0a813d2d0578d
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-27 15:32:13 +0900

    move to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  def require_admin
+    unless current_user.is_admin == true
+      flash[:error] = "You do not have the access to this section"
+
+      redirect_to root_url 
+    end
+  end
 end
