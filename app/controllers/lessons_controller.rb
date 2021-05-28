d501af1ecc92cd5ac4ed498cafc628733c171eeb
Saki-M-0831 <sakisaki0831@icloud.com>
2021-05-28 15:31:38 +0900

    retake

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,12 @@
 class LessonsController < ApplicationController
   def create
+    if Lesson.find_by(user_id: current_user.id, category_id: params[:category_id])
+      Lesson.find_by(user_id: current_user.id, category_id: params[:category_id]).destroy
+    end  
+
     @lesson = current_user.lessons.new(category_id: params[:category_id])
     @lesson.save
-    @lesson.create_activity(user_id:current_user.id)
+    @lesson.create_activity(user_id: current_user.id)
     
     redirect_to new_lesson_answer_url(@lesson)
   end
@@ -11,5 +15,11 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     @answers = @lesson.answers
     @category = @lesson.category
+  end
+
+  def continue
+    @lesson = Lesson.find(params[:id])
+
+    redirect_to new_lesson_answer_url(@lesson)
   end
 end
